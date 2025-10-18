efd9b96960ded773d6dc4e7de1395a152090ffc7
c-azb <carlos1997.cazb@gmail.com>
2025-10-18 13:56:21 -0300

    cicd

--- Backend_app/src/graph/states.py
+++ Backend_app/src/graph/states.py
@@ -1,6 +1,7 @@
 
 from typing import TypedDict
-from langchain.docstore.document import Document
+#from langchain.docstore.document import Document
+from langchain.schema import Document
 
 class State(TypedDict):
     article_search:str
